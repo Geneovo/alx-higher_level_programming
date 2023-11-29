2becde97c1c892a320e259ed4618fbf447218412
Geneovo <bodjuvwederhie@gmail.com>
2023-11-28 20:56:33 -0800

    omo

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def fizzbuzz():
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ", end=" ")
-        elif number % 3 == 0:
-            print("Fizz ", end=" ")
-        elif number % 5 == 0:
-            print("Buzz ", end=" ")
+    for c in range(1, 101):
+        if c % 3 == 0 and c % 5 != 0:
+            print("Fizz ", end='')
+        elif c % 5 == 0 and c % 3 != 0:
+            print("Buzz ", end='')
+        elif c % 5 == 0 and c % 3 == 0:
+            print("FizzBuzz ", end='')
         else:
-            print("{}".format(number), end=" ")
+            print("{}".format(c), end='')
