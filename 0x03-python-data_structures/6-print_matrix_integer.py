718f3dd3c359729c433d0da350821a6e974d681a
Geneovo <bodjuvwederhie@gmail.com>
2023-12-04 17:17:15 -0800

    python data structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for x in range(len(matrix)):
+        for z in range(len(matrix[x])):
+            print("{:d}".format(matrix[x][z]), end="")
+            if z != (len(matrix[x]) - 1):
+                print(" ", end="")
+
+        print("")
