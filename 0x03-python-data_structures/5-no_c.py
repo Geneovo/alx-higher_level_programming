77e8bca0dd8d5466bab3a49fcc7594e51f4fee88
Geneovo <bodjuvwederhie@gmail.com>
2023-12-04 17:08:06 -0800

    python data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    updated_str = ''
-    for x in my_string:
-        if x != 'c' and x != 'C':
-            updated_str += x
-        return (updated_str)
+    my_string_list = list(my_string)
+    idx_count = 0
+
+    for x in my_string_list:
+        if x == 'c' or x == 'C':
+            my_string_list[idx_count] = ""
+            idx_count += 1
+        return "".join(my_string_list)
