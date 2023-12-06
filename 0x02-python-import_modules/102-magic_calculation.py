210bc907caffb2f1968707c93508d7144e5f4da1
Geneovo <bodjuvwederhie@gmail.com>
2023-12-06 09:13:56 -0800

    python modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    add = __import__("magic_calculation_102").add
+    sub = __import__("magic_calculation_102").sub
+
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+        return c
+    else:
+            return sub(a, b)
