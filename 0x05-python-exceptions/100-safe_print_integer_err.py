66a82a6212e3d3169987d264cd837e0c101861d7
Geneovo <bodjuvwederhie@gmail.com>
2023-12-18 18:23:50 -0800

    Error Handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as x:
-        print("Exception: {}".format(x), file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(x))
         return False
