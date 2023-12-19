04304fb33a980c011a19c1089d5576cf2a6520f8
Geneovo <bodjuvwederhie@gmail.com>
2023-12-18 18:41:04 -0800

    Error Handling

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,12 +3,12 @@ def magic_calculation(a, b):
     total = 0
     for i in range(1, 4):
         try:
-            if i > 4:
+            if i > a:
                 raise Exception('Too far')
             total = (a ** b) / i
 
-        except:
-            total += b + a
+        except Exception:
+            total = b + a
             break
 
         return total
