9c1fff789d877232e671153490720c6b0af548b5
Geneovo <bodjuvwederhie@gmail.com>
2023-12-18 18:10:20 -0800

    Error handling

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        reture True
+    except Exception as x:
+        print("Exception: {}".format(x), file=sys,stderr)
+        return False
