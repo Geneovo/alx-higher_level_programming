4bb960172a54476202067781da797ce832566c9b
Geneovo <bodjuvwederhie@gmail.com>
2024-01-04 09:51:14 -0800

    Python more classes

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialization of this rectangle class
+        Args:
+            width (int): rectangle width
+            height (int): rectangle height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get method for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set method for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get method for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set method for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height - value
+
+    def area(self):
+        """Return the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the rectangle perimeter"""
+        return 2 * (self.__width + self.__height)
+
