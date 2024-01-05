31cd1c5a5b446f6de4272e7063bdce4b5521192e
Geneovo <bodjuvwederhie@gmail.com>
2024-01-05 06:14:06 -0800

    Python Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-magic_string = __import_-('100-magic_string').magic_string
+magic_string = __import__('100-magic_string').magic_string
 for x in range(10):
     print(magic_string())
