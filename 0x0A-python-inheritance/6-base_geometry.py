d27b62bcfde639c511c3838c5c2dd18efb95bdbc
Geneovo <bodjuvwederhie@gmail.com>
2024-01-08 08:02:02 -0800

    Python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A class with an empty area method"""
+    def area(self):
+        """Raises an exception with a specific message"""
+        raise Exception("area() is not implemented")
