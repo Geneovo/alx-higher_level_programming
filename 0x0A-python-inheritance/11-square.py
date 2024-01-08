097c9a2083639c54fb0889b38999efb4a5f73b7c
Geneovo <bodjuvwederhie@gmail.com>
2024-01-08 14:58:10 -0800

    Python inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Square class that inherits from the Rectangle class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """The Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialization of a square instance
+        Args:
+            size (int): size of the square
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns the square look"""
+        return "[Square] {}/{}".format(self.__width, self.__height)
