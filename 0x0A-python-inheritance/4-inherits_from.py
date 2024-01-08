22787c136a420fce41f1d380e6bcdd9898428e5c
Geneovo <bodjuvwederhie@gmail.com>
2024-01-08 15:11:08 -0800

    Python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,6 @@ def inherits_from(obj, a_class):
     Returns True if obj is an instance of a class inherited from a_class
     Otherwise False
     """
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
