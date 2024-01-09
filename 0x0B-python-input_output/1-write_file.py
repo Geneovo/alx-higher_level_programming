13b769d8be0bd48659534854d37c3d971329386c
Geneovo <bodjuvwederhie@gmail.com>
2024-01-09 15:55:58 -0800

    Python I/O

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that contains a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text files
+    Returns the number of characters written
+    Args:
+        filename (str): Name of the file to write
+        text (str): Text to write to the file
+    Returns:
+        int: Number of characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as f
+    return f.write(text)
