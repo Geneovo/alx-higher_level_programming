c555c8e769e112b936bc1b061015e4a33eae9b37
Geneovo <bodjuvwederhie@gmail.com>
2024-01-09 17:00:03 -0800

    Python I/O

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that returns the __dict__ attribute of an object"""
+
+
+def class_to_json(obj):
+    """Returns the __dict__ attribute of the object"""
+    return obj.__dict__
