e8b18ac9a957a22182ce8ffcfce4618724a67dff
Geneovo <bodjuvwederhie@gmail.com>
2024-02-14 08:01:02 -0800

    No Genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT TV_SHOWS.TITLE, TV_SHOW_GENRES.GENRE_ID
+SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_show_genres
   RIGHT JOIN tv_shows
   ON tv_shows.id = tv_show_genres.show_id
