1cb19443d346d9f2507c506120186d59a49ebdf0
Geneovo <bodjuvwederhie@gmail.com>
2024-03-11 16:25:01 -0700

    Argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,13 @@
 const { argv } = require('process');
 const argc = argv.length;
 
+const { argv } = require('process');
+const argc = argv.length;
+
 if (argc === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argc === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
