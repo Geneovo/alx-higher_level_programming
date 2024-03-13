46f6ca89a8216dd3335f5e58a618b4d6b9f8b6ca
Geneovo <bodjuvwederhie@gmail.com>
2024-03-13 05:54:05 -0700

    Sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
-}
+const newDict = Object.entries(dict).reduce((item, [k, v]) => {
+  item[v] = item[v] ? [...item[v], k] : [k];
+  return item;
+}, {});
+
 console.log(newDict);
