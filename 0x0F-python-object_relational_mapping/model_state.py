f57a092fd3987e0f6acea478779308e7baeddd85
Geneovo <bodjuvwederhie@gmail.com>
2024-03-22 15:23:56 -0700

    Model State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" contains the class definition of a State and an instance Base """
+""" contains the class definition of a state and an instance Base """
 import sys
 from model_state import Base, State
 
