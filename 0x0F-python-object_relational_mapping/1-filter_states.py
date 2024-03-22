8856f7920f09ade6c039aafb0bdd79beafcad7ce
Geneovo <bodjuvwederhie@gmail.com>
2024-03-22 14:49:17 -0700

    Filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
     cur.close()
-g   db.close()
+    db.close()
